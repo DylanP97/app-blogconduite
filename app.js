9a7a9ee1016dbd6a7656ccfb151acda36582a757
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-01-12 17:41:32 +0100

    Initial commit 8

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ mongoose.connect(process.env.MONGO_SECRET,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
+app.use('/', express.static('dist'))
+
 // cors, helmet, cookie parser
 
 const app = express();
@@ -50,40 +52,40 @@ app.use(cookieParser());
 
 
 // stripe
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
-const storeItems = new Map([
-  [1, { priceInCents: 900, name: 'Accès Apprentissage Conduite Alexandre'}],
-  [2, { priceInCents: 2000, name: 'Produit 2'}],
-])
-app.post('/create-checkout-session', async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
-      line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
-        return {
-          price_data: {
-            currency: 'eur',
-            product_data: {
-                name: storeItem.name
-            },
-            unit_amount: storeItem.priceInCents
-          },
-          quantity: item.quantity
-        }
-      }),
-      success_url: `${process.env.FRONTEND_URL}/inscription`,
-      cancel_url:`${process.env.FRONTEND_URL}/inscription`,
-    })
-  res.json({ url: session.url })
+// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+// const storeItems = new Map([
+//   [1, { priceInCents: 900, name: 'Accès Apprentissage Conduite Alexandre'}],
+//   [2, { priceInCents: 2000, name: 'Produit 2'}],
+// ])
+// app.post('/create-checkout-session', async (req, res) => {
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ['card'],
+//       mode: 'payment',
+//       line_items: req.body.items.map(item => {
+//         const storeItem = storeItems.get(item.id)
+//         return {
+//           price_data: {
+//             currency: 'eur',
+//             product_data: {
+//                 name: storeItem.name
+//             },
+//             unit_amount: storeItem.priceInCents
+//           },
+//           quantity: item.quantity
+//         }
+//       }),
+//       success_url: `${process.env.FRONTEND_URL}/inscription`,
+//       cancel_url:`${process.env.FRONTEND_URL}/inscription`,
+//     })
+//   res.json({ url: session.url })
 
 
-  } catch (e) {
-      res.status(500).json({ error: e.message })
-  }
+//   } catch (e) {
+//       res.status(500).json({ error: e.message })
+//   }
 
-})
+// })
 
 // jwt
 app.get('/jwt', auth, (req, res, next) => {
