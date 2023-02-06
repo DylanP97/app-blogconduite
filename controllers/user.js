67aaaedd0ea2f30256a1aa6b0fd59636a4877c18
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-02-06 15:37:31 +0100

    Initial commit 38

--- controllers/user.js
+++ controllers/user.js
@@ -106,23 +106,8 @@ exports.login = async (req, res, next) => {
 };
 
 exports.logout = (req, res, next) => {
-  const user = UserModel.findOne({ _id: req.body.data })
-  const token = createToken(user._id);
-
-  console.log(req.data)
-  console.log(token)
-
   res.cookie('jwt', ' ', { maxAge: 1 });
-  res.status(200).json({ message: "User logged out successfully" });
-  // res.redirect('/');
-    // res.clearCookie("jwt", token, {
-    //   domain: "https://app-blogconduite-dylanp97.onrender.com/",
-    //   path: "/",
-    //   httpOnly: true,
-    //   sameSite: "None",
-    //   secure: "true",
-    // });
-    // res.status(200).json({ message: "User logged out successfully" });
+  res.redirect('/');
 };
 
 exports.forgotpassword = async (req, res, next) => {
