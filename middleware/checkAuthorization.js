cc0d241c3280be386b61deb84df8b88152faa9e1
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-03-13 17:44:31 +0100

    Initial commit 67

--- middleware/checkAuthorization.js
+++ middleware/checkAuthorization.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const { generateAccessToken } = require("../utils/generateTokens");
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const generateAccessToken = async (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET, { expiresIn: '30s' });
+  return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET, { expiresIn: '30s' , sameSite: 'None' });
 };
 
 const generateRefreshToken = async (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' , sameSite: 'None' });
 };
 
 module.exports = { generateAccessToken, generateRefreshToken };
